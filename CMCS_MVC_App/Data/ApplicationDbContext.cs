56dd0e9b2932089e5267aaa1a11b428ab289cdcf
ST10266848 <ST10266848@vcconnect.edu.za>
2024-11-19 11:06:24 +0200

    Commit 1: Started work on the final part of the Prog6212 POE
    - Pushed the Part 2 version of the CMCS MVC application to GitHub in preparation for the final part's additions.

---
+++ CMCS_MVC_App/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CMCS_MVC_App.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMCS_MVC_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {  
+        }
+
+        public DbSet<Claim> Claims { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        
+    }
+}
---
+++ CMCS_MVC_App/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMCS_MVC_App.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        //Code Attribution for the implementation of AspNetCore Identity, Roles, Authorization
+        //,ApplicationUser (adding additional fields to AspNetUsers table):
+        //Author: Digital TechJoint
+        //Source: Two YouTube Videos
+        //Video 1 Link: https://www.youtube.com/watch?v=ghzvSROMo_M
+        //Video 2 Link: https://www.youtube.com/watch?v=qvsWwwq2ynE
+        //Date Accessed: 15 October 2024
+
+
+        //Attributes for each of the below properties are contained with Register.cshtml.cs
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string BankName { get; set; } = null!;
+        public string AccountHolderName { get; set; } = null!;
+        public string AccountNumber { get; set; }
+
+
+    }
+}
